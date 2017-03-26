138902077bc693cb120f56287b7ef5c0b609a6ab
dnguyenv <dnguyenv@us.ibm.com>
2017-03-26 12:31:27 -0400

    revised code structure

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
 // You can change the voice of the robot to your favorite voice.
-exports.voice = 'en-US_LisaVoice';
+exports.voice = 'en-US_AllisonVoice';
 // Some of the available options are:
 // en-US_AllisonVoice
 // en-US_LisaVoice
 // en-US_MichaelVoice (the default)
 
 //Credentials for Watson Text to Speech service
-exports.TTSPassword = 'xxxxxx' ;
-exports.TTSUsername = 'xxxxxx' ;
+exports.TTSPassword = 'xxxxxxxxxx' ;
+exports.TTSUsername = 'xxxxxxxxxx' ;
+
+// Configuration for SC-SR04 sensor
+exports.Sensor = {
+    echoPin: 24,
+    triggerPin: 23,
+    timeout: 750,
+    delay: 60,
+    rate: 5
+};
+// Text to speak out.
+exports.TextToSpeak = "Wow, you are too close to me, I need some privacy please!";
--- speak.js
+++ speak.js
@@ -1,87 +1,75 @@
-var ECHO_PIN = 24,
-    TRIG_PIN = 23;
-
 var watson = require('watson-developer-cloud');
 var config = require("./config.js")
 var exec = require('child_process').exec;
 var fs = require('fs');
-var text_to_speak = "This is a test";
 var statistics = require('math-statistics');
 var usonic = require('mmm-usonic');
 
-var text_to_speech = watson.text_to_speech({
-    username: config.TTSUsername,
-    password: config.TTSPassword,
-    version: 'v1'
-});
-
-var params = {
-    text: text_to_speak,
-    voice: config.voice,
-    accept: 'audio/wav'
-};
-
-/*
-tempStream = text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav')).on('close', function() {
-    var create_audio = exec('aplay output.wav', function(error, stdout, stderr) {
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
+/* Convert input text to speech using IBM Watson text to speech service */
+var speak = function(text){
+    var params = {
+        text: text,
+        voice: config.voice,
+        accept: 'audio/wav'
+    };
+    var text_to_speech = watson.text_to_speech({
+        username: config.TTSUsername,
+        password: config.TTSPassword,
+        version: 'v1'
     });
-});*/
 
+    /* Streaming the resulting audio to file and play the file using aplay */
 
+    text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav')).on('close', function() {
+        var create_audio = exec('aplay output.wav', function(error, stdout, stderr) {
+            if (error !== null) {
+                console.log('Error occurred while playing back: ' + error);
+            }
+        });
+    });
+}
 
-
-var print = function(distances) {
+var processDistance = function(distances) {
     var distance = statistics.median(distances);
 
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
 
     if (distance < 0) {
-        process.stdout.write('Error: Measurement timeout.\n');
+        process.stdout.write('Somthing wrong with measurement ...\n');
     } else {
         process.stdout.write('Distance: ' + distance.toFixed(2) + ' cm');
+        var privacy = distance.toFixed(2);
+        /*Speak up if the distance to the clear by object is less than 10 cm */
+        if (privacy < 10.00){
+          speak(config.TextToSpeak);
+        }
     }
 };
-
-var initSensor = function(config) {
+/* Initiate the sensor and do the measurement in every config.delay*/
+var startMeasuring = function(config) {
     var sensor = usonic.createSensor(config.echoPin, config.triggerPin, config.timeout);
-
     console.log('Config: ' + JSON.stringify(config));
-
     var distances;
-
     (function measure() {
+        /* Refresh the distances array if enough distances are added in */
         if (!distances || distances.length === config.rate) {
-            if (distances) {
-                print(distances);
-            }else{
-              console.log('Khong co distance');
-            }
-
+            if (distances) processDistance(distances);
             distances = [];
-        }
-
+        };
+        /* Execute the measurement every config.delay ms*/
         setTimeout(function() {
-            console.log('vao setTimeout: ', distances.length);
             distances.push(sensor());
             measure();
         }, config.delay);
     }());
 };
 
+/* Main entry point */
 usonic.init(function(error) {
     if (error) {
         console.log(error);
     } else {
-        initSensor({
-            echoPin: ECHO_PIN,
-            triggerPin: TRIG_PIN,
-            timeout: 750,
-            delay: 60,
-            rate: 5
-        });
+        startMeasuring(config.Sensor);
     }
 });
